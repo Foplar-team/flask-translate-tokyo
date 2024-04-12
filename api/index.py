a1d246441ba73e97a695e229eda02bb4ce2172e6
moon-jong <dksjfl@naver.com>
2024-04-12 18:40:12 +0900

    change script key

--- api/index.py
+++ api/index.py
@@ -9,12 +9,12 @@ CORS(app)  # 모든 도메인에서의 요청을 허용하도록 설정
 
 @app.route('/api/translate',methods=['GET', 'POST'])
 def translate():
-    # data['data'] : 번역 할 스크립트가 들어있는 객체
+    # data['script'] : 번역 할 스크립트가 들어있는 객체
     # data['dest'] : 번역 할 타겟 언어가 들어있는 객체 ( en | ko | es )
     start_time = time.time()
     data = request.json
     translator = Translator()
-    result = translator.translate(data['data'], dest=data['dest'])  #
+    result = translator.translate(data['script'], dest=data['dest'])  #
     print(f'total translate time : {time.time() - start_time}')
     return result.text
 
